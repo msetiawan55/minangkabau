05dceaa13d70f49d4bd8e2270ca9798929d1ea91
msetiawan55 <54870752+msetiawan55@users.noreply.github.com>
2023-08-05 10:00:05 +0700

    app.py

--- app.py
+++ app.py
@@ -57,39 +57,9 @@ add_bg_from_local('bunda.png')
 
 def get_data(url):
 	resp = requests.get(url)
-	return resp.json()
+	return resp.json()       
 
-
-#sidebar
-with st.sidebar:
-    choose = option_menu("Galeri Aplikasi", ["Translasi", "Tentang" ],
-                         icons=['translate', 'book','house'],
-                         menu_icon="app-indicator", default_index=0,
-                         styles={
-        "container": {"padding": "5!important", "background-color": "#fafafa"},
-        "icon": {"color": "#C21616", "font-size": "25px"}, 
-        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "#FFB90F"},
-    }
-    )
-
-
-
-if choose == "Tentang":
-    col1, col2 = st.columns( [100, 0.2])
-    with col1:               # To display the header text using css style
-        st.markdown(""" <style> .font {
-        font-size:35px ; font-family: 'Cooper Black'; color: #C21616; text-align: center;} 
-        </style> """, unsafe_allow_html=True)
-        st.markdown('<p class="font">TENTANG KAMI</p>', unsafe_allow_html=True)    
-        st.markdown('<style>body {background-color: #f5f5f5;} p {font-size:20px ;} p {text-align: justify;}</style>', unsafe_allow_html=True)
-        kalimat = "Assalamualaikum! Translasi Bahasa Minangkabau merupakan kamus bahasa Minangkabau. aplikasi ini dapat melakukan terjemahan bahasa Minangkabau ke bahasa indonesia atau bahasa indonesia ke bahasa Minangkabau secara online. Anda dapat mempelajari kata dan kosakata yang ada di dalam bahasa Minangkabau secara online. Kami juga menyediakan aksara Lampung untuk dipelajari. Aplikasi ini dapat menjadi referensi anda dalam belajar bahasa Minangkabau. Mari kita lestarikan budaya Minangkabau dengan tetap belajar dan mengetahui bahasa Minangkabau."
-        st.markdown(kalimat, unsafe_allow_html=True)
-
-
-        
-
-elif choose == "Translasi":
+if "Translasi":
     col1, col2 = st.columns( [100, 0.2])
     with col1:               # To display the header text using css style
 
